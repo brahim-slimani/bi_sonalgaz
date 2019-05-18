e96cd0725a0bd98002bd98fcc1d03f9cdfa58478
Slimani-Ibrahim <slimani.ibr@gmail.com>
2019-05-18 04:02:55 +0100

    fragment displaying charts cb

--- app/src/main/java/com/slimani/bi_sonalgaz/adhoc/chartsFragments/ColumnbarFrgament.java
+++ app/src/main/java/com/slimani/bi_sonalgaz/adhoc/chartsFragments/ColumnbarFrgament.java
@@ -44,7 +44,7 @@ public class ColumnbarFrgament extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         view = inflater.inflate(R.layout.fragment_columnbarchart, container, false);
 
         final AnyChartView anyChartView = view.findViewById(R.id.any_chart_view);
@@ -56,18 +56,6 @@ public class ColumnbarFrgament extends Fragment {
         anyChartView.setLicenceKey("b.slimani@esi-sba.dz-b0e90d3d-68aabd2c");
         ChartCredits cc = cartesian.credits();
         cc.text("ELIT-SONALGAZ");
-
-        /*
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("Rouge", 80540));
-        data.add(new ValueDataEntry("Foundation", 94190));
-        data.add(new ValueDataEntry("Mascara", 102610));
-        data.add(new ValueDataEntry("Lip gloss", 110430));
-        data.add(new ValueDataEntry("Lipstick", 128000));
-        data.add(new ValueDataEntry("Nail polish", 143760));
-        data.add(new ValueDataEntry("Eyebrow pencil", 170670));
-        data.add(new ValueDataEntry("Eyeliner", 213210));
-        data.add(new ValueDataEntry("Eyeshadows", 249980));*/
 
         DataManager dataManager = new DataManager();
 
@@ -187,108 +175,6 @@ public class ColumnbarFrgament extends Fragment {
 
 
 
-/*
-
-        Cartesian barChart = AnyChart.bar();
-
-        barChart.animation(true);
-
-        barChart.padding(10d, 20d, 5d, 20d);
-
-        barChart.yScale().stackMode(ScaleStackMode.VALUE);
-
-        barChart.yAxis(0).labels().format(
-                "function() {\n" +
-                        "    return Math.abs(this.value).toLocaleString();\n" +
-                        "  }");
-
-        barChart.yAxis(0d).title("Revenue in Dollars");
-
-        barChart.xAxis(0d).overlapMode(LabelsOverlapMode.ALLOW_OVERLAP);
-
-        Linear xAxis1 = barChart.xAxis(1d);
-        xAxis1.enabled(true);
-        xAxis1.orientation(Orientation.RIGHT);
-        xAxis1.overlapMode(LabelsOverlapMode.ALLOW_OVERLAP);
-
-        barChart.title("Cosmetic Sales by Gender");
-
-        barChart.interactivity().hoverMode(HoverMode.BY_X);
-
-        barChart.tooltip()
-                .title(false)
-                .separator(false)
-                .displayMode(TooltipDisplayMode.SEPARATED)
-                .positionMode(TooltipPositionMode.POINT)
-                .useHtml(true)
-                .fontSize(12d)
-                .offsetX(5d)
-                .offsetY(0d)
-                .format(
-                        "function() {\n" +
-                                "      return '<span style=\"color: #D9D9D9\">$</span>' + Math.abs(this.value).toLocaleString();\n" +
-                                "    }");
-
-        List<DataEntry> seriesData = new ArrayList<>();
-        seriesData.add(new CustomDataEntry("Nail polish", 5376, -229));
-        seriesData.add(new CustomDataEntry("Eyebrow pencil", 10987, -932));
-        seriesData.add(new CustomDataEntry("Rouge", 7624, -5221));
-        seriesData.add(new CustomDataEntry("Lipstick", 8814, -256));
-        seriesData.add(new CustomDataEntry("Eyeshadows", 8998, -308));
-        seriesData.add(new CustomDataEntry("Eyeliner", 9321, -432));
-        seriesData.add(new CustomDataEntry("Foundation", 8342, -701));
-        seriesData.add(new CustomDataEntry("Lip gloss", 6998, -908));
-        seriesData.add(new CustomDataEntry("Mascara", 9261, -712));
-        seriesData.add(new CustomDataEntry("Shampoo", 5376, -9229));
-        seriesData.add(new CustomDataEntry("Hair conditioner", 10987, -13932));
-        seriesData.add(new CustomDataEntry("Body lotion", 7624, -10221));
-        seriesData.add(new CustomDataEntry("Shower gel", 8814, -12256));
-        seriesData.add(new CustomDataEntry("Soap", 8998, -5308));
-        seriesData.add(new CustomDataEntry("Eye fresher", 9321, -432));
-        seriesData.add(new CustomDataEntry("Deodorant", 8342, -11701));
-        seriesData.add(new CustomDataEntry("Hand cream", 7598, -5808));
-        seriesData.add(new CustomDataEntry("Foot cream", 6098, -3987));
-        seriesData.add(new CustomDataEntry("Night cream", 6998, -847));
-        seriesData.add(new CustomDataEntry("Day cream", 5304, -4008));
-        seriesData.add(new CustomDataEntry("Vanila cream", 9261, -712));
-
-        Set set = Set.instantiate();
-        set.data(seriesData);
-        Mapping series1Data = set.mapAs("{ x: 'x', value: 'value' }");
-        Mapping series2Data = set.mapAs("{ x: 'x', value: 'value2' }");
-        Mapping series3Data = set.mapAs("{ x: 'x', value: 'value3' }");
-
-        Bar series1 = barChart.bar(series1Data);
-        series1.name("Females")
-                .color("HotPink");
-        series1.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER);
-
-        Bar series2 = barChart.bar(series2Data);
-        series2.name("Males");
-        series2.tooltip()
-                .position("left")
-                .anchor(Anchor.RIGHT_CENTER);
-
-        Bar series3 = barChart.bar(series3Data);
-        series3.name("Mixte");
-        series3.tooltip()
-                .position("left")
-                .anchor(Anchor.RIGHT_CENTER);
-
-        barChart.legend().enabled(true);
-        barChart.legend().inverted(true);
-        barChart.legend().fontSize(13d);
-        barChart.legend().padding(0d, 0d, 20d, 0d);
-
-        anyChartView.setChart(barChart);
-
-
-        System.out.println(dataJS);
-
-
-*/
 
         return view;
 
